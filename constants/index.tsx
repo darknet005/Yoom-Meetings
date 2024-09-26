98aaaaa575190162863df264f41216b496d55e3e
darknat005 <prabhuchinmay0@gmail.com>
2024-09-26 17:32:03 +0530

    third commit

---
+++ constants/index.tsx
@@ -0,0 +1,32 @@
+export const sidebarlinks = [
+    {
+        label : 'Home',
+        route : '/',
+        imgUrl : '/icons/Home.svg',
+              
+    },
+    {
+        label : 'Upcoming',
+        route : '/upcoming',
+        imgUrl : '/icons/Upcoming.svg',
+              
+    },
+    {
+        label : 'Previous',
+        route : '/previous',
+        imgUrl : '/icons/previous.svg',
+              
+    },
+    {
+        label : 'Recordings',
+        route : '/recording',
+        imgUrl : '/icons/Video.svg',
+              
+    },
+    {
+        label : 'Personal Room',
+        route : '/personal-room',
+        imgUrl : '/icons/add-personal.svg',
+              
+    },
+]
